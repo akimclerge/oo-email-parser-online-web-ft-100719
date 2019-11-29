f92baad70f918e180ff100865ea716091a09ed83
Akim Clerge <akimclerge@gmail.com>
2019-11-29 23:50:24 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,16 +1,1 @@
-class EmailParser 
-  attr_accessor :name, :csv_emails
-  
-  def initialize(csv_emails)
-
-    @csv_emails = csv_emails
-  end 
-
-
-  def parse 
-    csv_emails.split.collect do |address|
-      address.split(',') 
-    end
-    .flatten.uniq 
-  end 
-end 
+EMAIL_ADDRESSES = "JOHN@DOE, p"
